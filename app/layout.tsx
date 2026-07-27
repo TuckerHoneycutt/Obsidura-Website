e0e8a8075063784351a80a372676122f08f05e0a
TuckerHoneycutt <tuckerpersonalmail@gmail.com>
2026-07-27 15:59:58 -0500

    working changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col paper-grain">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem={false}
           disableTransitionOnChange
         >
